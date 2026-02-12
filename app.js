1164df123325fd828c953cc07ba6b83ae8b81403
Bruno <brunoariel.dev@gmail.com>
2026-02-12 19:41:35 -0300

    second-commit8

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use('/jobs', jobsRouter) // <---- router con todas las rutas
 
 
-if(process.env.NODE_ENV !== 'production') {
+if(!process.env.NODE_ENV) {
     app.listen(PORT, () => { 
     console.log(`servidor escuchado en http://localhost:${PORT}`);
 });
--- models/jobs.js
+++ models/jobs.js
@@ -12,7 +12,7 @@ export class JobModel {
         }
         if (technology) {
             filteredJobs = filteredJobs.filter(job =>
-                job.tecnologias.includes(technology)
+                job.data.technology.includes(technology)
             );
         }
         
